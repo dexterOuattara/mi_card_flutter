c84f10b03b22865d96fc13c8f9ae52d128fb8565
dexterOuattara <31624715+dexterOuattara@users.noreply.github.com>
2022-07-08 19:04:26 +0000

    mi card
    mi card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,53 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
+  return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(
+          title: Text('Should I Work Or No ?'),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: WorkPage(),
       ),
     ),
   );
+}
+
+class WorkPage extends StatefulWidget {
+  const WorkPage({Key key}) : super(key: key);
+
+  @override
+  State<WorkPage> createState() => _WorkPageState();
+}
+
+class _WorkPageState extends State<WorkPage> {
+  int workNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      workNumber = Random().nextInt(4) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+                print('workNumber = $workNumber');
+              },
+              child: Image.asset('images/worker$workNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
